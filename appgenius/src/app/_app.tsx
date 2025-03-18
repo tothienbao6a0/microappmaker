d90793d6743c9fb90f2ac95bbc7fdd599725eb26
Bao To <tothienbao6a0@gmail.com>
2025-03-18 05:21:43 -0400

    idk

--- appgenius/src/app/_app.tsx
+++ appgenius/src/app/_app.tsx
@@ -3,8 +3,15 @@ import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    // Force dark mode
     document.documentElement.classList.add('dark');
     document.documentElement.style.colorScheme = 'dark';
+    
+    // Remove any light mode classes
+    document.documentElement.classList.remove('light');
+
+    // Debugging: Log the current theme
+    console.log('Current theme:', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
   }, []);
 
   return (
